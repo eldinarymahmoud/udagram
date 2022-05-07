9eb81eb073ee0bd924044637fc7bb0d6460b0caa
Mahmoud Eldinary <eldinarymahmoud@gmail.com>
2022-05-07 13:26:48 +0200

    Adjusting environment variables

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -9,11 +9,11 @@ export const config = {
   dbport: Number(process.env.PORT_DB),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
+  aws_region: process.env.AWS_DEFAULT_REGION,
+  aws_profile: process.env.AWS_ACCESS_KEY_ID,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.AWS_SECRET_ACCESS_KEY,
   },
 };
